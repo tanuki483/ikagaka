a64f13d5216cf28734b35ad13ff0b47852b9600c
tanuki483 <sougo_summer@yahoo.co.jp>
2026-04-10 00:19:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,22 @@ const CACHE_NAME = 'ikagaka-v1';
 const urlsToCache = [
   './',
   './index.html',
-  // 必要なJS、CSS、vendorフォルダ内のファイルなどを列挙（BrowserFSなど重いので最低限）
-  // または動的キャッシュ戦略を使う
+  './js/ikagakaConfig.js',
+  './css/',
+  './vendor/',
+  './img/'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
